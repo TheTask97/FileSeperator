b092f083c2af625a02d4d88e4cf94db2348ec6b1
Armin Lezic <49408788+TheTask97@users.noreply.github.com>
2019-05-24 22:49:06 +0200

    Add files via upload

---
+++ FileSeperator.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.io.*;
+
+public class FileSeperator {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		String[] sigma = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+				"s", "t", "u", "v", "w", "x", "y", "z" };
+		String[] sigmaGrossesBoy = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+				"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+		File f = new File("C:\\BaranTest\\Lesen\\left.txt");
+		String s;
+
+		for (int i = 0; i < sigma.length; i++) {
+			BufferedReader bfr = new BufferedReader(new FileReader(f));
+			try {
+				BufferedWriter bfw = new BufferedWriter(
+						new FileWriter("C:\\BaranTest\\schreiben\\" + sigma[i] + "LeftText.txt", true));
+				System.out.println(sigmaGrossesBoy[i]);
+
+				while ((s = bfr.readLine()) != null) {
+					if (s.startsWith(sigma[i]) || s.startsWith(sigmaGrossesBoy[i])) {
+						bfw.write(s + "\r\n");
+						bfw.flush();
+					}
+				}
+				bfr.close();
+				bfw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
