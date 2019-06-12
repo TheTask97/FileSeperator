192cfc2c6181bd878ac796dcd0ee0fddd490ebe8
Armin Lezic <49408788+TheTask97@users.noreply.github.com>
2019-06-12 13:15:34 +0200

    Update FileSeperator.java

--- FileSeperator.java
+++ FileSeperator.java
@@ -11,9 +11,9 @@ public class FileSeperator {
 		String readPath;
 		String[] sigma = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
 				"s", "t", "u", "v", "w", "x", "y", "z" };
-		String[] sigmaGrossesBoy = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+		String[] sigmaCap = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
 				"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-		File f = new File("C:\\BaranTest\\Lesen\\left.txt");
+		File f = new File("C:\\Test\\Read\\left.txt");
 		String s;
 
 		for (int i = 0; i < sigma.length; i++) {
@@ -21,10 +21,10 @@ public class FileSeperator {
 			try {
 				BufferedWriter bfw = new BufferedWriter(
 						new FileWriter(createPath + sigma[i] + ".txt", true));
-				System.out.println(sigmaGrossesBoy[i]);
+				System.out.println(sigmaCap[i]);
 
 				while ((s = bfr.readLine()) != null) {
-					if (s.startsWith(sigma[i]) || s.startsWith(sigmaGrossesBoy[i])) {
+					if (s.startsWith(sigma[i]) || s.startsWith(sigmaCap[i])) {
 						bfw.write(s + "\r\n");
 						bfw.flush();
 					}
